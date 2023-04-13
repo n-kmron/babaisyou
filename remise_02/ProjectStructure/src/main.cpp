02b3bbf06e08df428db37b57511cdfad237975c4
n-kmron <cameronnoupoue@gmail.com>
2023-04-13 11:22:32 +0200

    adds what to do in main

--- remise_02/ProjectStructure/src/main.cpp
+++ remise_02/ProjectStructure/src/main.cpp
@@ -19,8 +19,6 @@ int main()
 // TODO : refractor LevelMechanics
 // TODO : levels dans -> levels/default & levels/saves
 
-//par soucis de pratique, en version console, on demande pour save uniquement lorsqu'un niveau est gagné
-
 //mentionner le numero du level au début du file
 
 //quand on lance le jeu, askLevel() qui demande le niveau à lancer (save ou default, ...)
